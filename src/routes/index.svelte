bb838678c3e68dda4760e83b25e05382c9feef4a
Victor Hugo Aguilar <37599330+VictorHugoAguilar@users.noreply.github.com>
2022-08-11 20:42:42 +0200

    add input city for search

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,22 +3,98 @@
 	import WeatherFooter from '../components/weather-footer.svelte';
 	import WeatherIcon from '../components/weather-icon.svelte';
 
-	const jamonConQueso = getWeatherFrom();
+	let response = getWeatherFrom('Murcia');
+
+	function handlerChangeInput(event) {
+		const { value } = event.target;
+		if (value.length > 0) {
+			response = getWeatherFrom(value);
+		}
+	}
 </script>
 
-{#await jamonConQueso then weather}
-	<section>
-		<h1>{weather.locationName}</h1>
-		<h2>{weather.temperature}º</h2>
-		<h3>{weather.conditionText}</h3>
-		<WeatherIcon text={weather.conditionText} icon={weather.conditionIcon} />
-	</section>
-	<WeatherFooter />
-{/await}
+<div class="index">
+	<div class="header">
+		<h1>
+			<slot name="title">
+				<span>App Weather</span>
+			</slot>
+		</h1>
+	</div>
+
+	<div class="searchCity">
+		<div class="form-group">
+			<label for="city">Search City</label>
+			<input
+				on:change={handlerChangeInput}
+				type="text"
+				class="form-control"
+				id="city"
+				placeholder="Enter city"
+			/>
+		</div>
+	</div>
+
+	<div class="center">
+		{#await response then weather}
+			<section>
+				<div class="nameCity">
+					<h1>{weather.locationName}</h1>
+				</div>
+				<div class="temperatureCity"> 
+					<h2>{weather.temperature}º</h2>
+					<h3>{weather.conditionText}</h3>
+				</div>					
+				<WeatherIcon text={weather.conditionText} icon={weather.conditionIcon} />
+			</section>
+			<WeatherFooter />
+		{/await}
+	</div>
+</div>
 
 <style>
+	.index {
+		margin: 0;
+	}
+
+	.header {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin-bottom: 1rem;
+	}
+
+	.searchCity {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin-bottom: 1rem;
+	}
+
+	.center{
+		display: flex;
+		margin-bottom: 1rem;
+	}
+
+	.nameCity{
+		width: 100%;
+		display: flex;
+		justify-content: left;
+		align-items: center;
+		margin-bottom: 1rem;
+	}
+
+	.temperatureCity{
+		width: 100%;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		margin-bottom: 1rem;
+	}
+
 	section {
 		padding: 16px;
+		width: 100%;
 	}
 
 	h1 {
@@ -39,8 +115,5 @@
 	h3 {
 		font-weight: 700;
 		transform: rotate(-90deg);
-		position: absolute;
-		top: 56px;
-		right: 12px;
 	}
 </style>
